9b0d20aa558926e787c2fb19931ab759a1f69298
edoC16 <43505068+edoC-16@users.noreply.github.com>
2020-11-05 20:14:09 -0800

    updated projects page

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "./projects.css"
+
+
+
 
 function projects(props) {
     return (
         <div>
-            Projects page! 
+            <div className="projectTitle" style={{}}>projects.</div>
+            <p className="projectDesc">
+                Each quarter, our teams take on projects
+                for clients or for developing the team's
+                coding skills.
+            </p>
+            <p className="projectWow">
+                In all our projects, we strive for wow.
+                We imagine, innovate, and constantly
+                learn new things to push the limits of what
+                we can do.
+            </p>
         </div>
     )
 }
