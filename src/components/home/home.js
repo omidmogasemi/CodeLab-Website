408a9548e42e44363ffa7a0ddcd381649115880f
DanielKim12 <kimgukil2@example.com>
2020-11-05 20:10:24 -0800

    updated home_page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,9 +3,21 @@ import React from "react";
 function home(props) {
     return (
         <div>
-            Home page! 
+        <label>
+            Home Page!
+            <img src="https://media-exp1.licdn.com/dms/image/C560BAQFZQpzhARjiRg/company-logo_200_200/0?e=2159024400&v=beta&t=RwjquZdpfq-lfaJ4dTNf_AFepRswZuSt8L0eS7nZD40"/>
+            <logo></logo>
+            <label>
+                We’re a team of driven, 
+                exploring developers at UC Davis, 
+                building projects for clients.
+            </label>
+            <label>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFtndUuH8KTDrEYKGCnyn1-ZOGgZtJ-treQQ&usqp=CAU"/>
+            </label>
+        </label>
         </div>
     )
 }
 
-export default home
+export default home;
