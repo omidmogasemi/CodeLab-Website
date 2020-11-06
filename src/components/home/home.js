8c1c32815377bbb517ad4cf941429e945bf1210f
Omid Mogasemi <omidmogasemi@gmail.com>
2020-11-05 21:17:59 -0800

    Fixed grid formatting

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,36 +1,27 @@
 import React from "react";
-import { Grid, Link } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import styles from "../../css/home.css"
+import img from "./logo.svg"
 
 function home(props) {
     return (
      <Grid container> 
-
-        <Grid container item xs={6}>
-            Welcome to the CodeLab!
-            <Grid item>
-                <div>
-                <label>
-                Home page!
-                <img src="https://media-exp1.licdn.com/dms/image/C560BAQFZQpzhARjiRg/company-logo_200_200/0?e=2159024400&v=beta&t=RwjquZdpfq-lfaJ4dTNf_AFepRswZuSt8L0eS7nZD40"/>
-                <logo></logo>
-                </label>
-                </div>
+        <Grid container item xs={1}/>
+        <Grid container item xs={4}>
+            <img class="imagestyle" src={img}/>
+            <label class="toptext">
+            We’re a team of driven, exploring developers at UC Davis, building projects for clients.
+            </label>
+        </Grid>
+        <Grid container item xs={2} />
+            <Grid container item xs={4}>
+                <Grid item>
+                    <label>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFtndUuH8KTDrEYKGCnyn1-ZOGgZtJ-treQQ&usqp=CAU"/>
+                    </label>
+                </Grid>
             </Grid>
-            <Grid item>
-                <label>
-                We’re a team of driven, 
-                exploring developers at UC Davis, 
-                building projects for clients.
-                </label>
-            </Grid>
-            </Grid>
-             <Grid container item justify="flex-end" xs={6}>
-            <Grid item>
-                <label>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFtndUuH8KTDrEYKGCnyn1-ZOGgZtJ-treQQ&usqp=CAU"/>
-                </label>
-            </Grid>
-       </Grid>
+        <Grid container item xs={1} />
     </Grid>
     )
 }
