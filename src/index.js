a99902fd6ac7d7db62c2560190744661ab2675b3
James Junaidi <james.junaidi@gmail.com>
2020-11-05 20:15:24 -0800

    james commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
+// james was here
 const history = createBrowserHistory();
 
 ReactDOM.render(
