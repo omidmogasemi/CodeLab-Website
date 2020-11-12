3d9a2ae4dc40e85912710cb4ec65c87eb5f991fd
Tom Abraham <tomabraham6279@gmail.com>
2020-11-12 13:20:33 -0800

    Implemented Grid on Week 5 and 6 tasks

--- src/components/getInvolved/getInvolved.js
+++ src/components/getInvolved/getInvolved.js
@@ -1,56 +1,110 @@
 import React from "react";
+import { Grid, Link } from "@material-ui/core";
 
 function getInvolved(props) {
     return (
-        <div>
-            <header className="getinvolved">get involved.</header> 
-            <p className="getinvolvedsub">There are many different ways to get involved and we would love for you to join us.</p>
+        <Grid container direction="column">
+            <div>
+                
+                <Grid item container>
+                    <Grid item xs={1} />
+                    <Grid item xs={6}>
+                        <header className="getinvolved">get involved.</header>
+                    </Grid>
+                </Grid>
+
+                <Grid item container>
+                    <Grid item xs={1} />
+                    <Grid item xs={6}>
+                        <p className="getinvolvedsub">There are many different ways to get involved and we would love for you to join us.</p>
+                    </Grid>
+                </Grid>
+
+                <Grid item container>
+                    <Grid item xs={1} />
+                    <Grid item xs={6}>
+                        <header className="projectteams">Project Teams</header>
+                    </Grid>
+                </Grid>
+
+                <Grid item container>
+                    <Grid item xs={1} />
+                    <Grid item xs={5}>
+                        <div class="clientbox">
+                            <header class="clientheader">Client Project</header>
+                            <Grid item xs={11}>
+                                <p className="clienttext">
+                                    <br></br>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat sagittis integer placerat quis metus, nisl, tellus. Sed diam et massa ante id egestas nunc lacus.
+                                    <br></br>
+                                    <br></br>
+                                     Requirements:  
+                                </p>
+                            </Grid>
+                            
+                             <ul className="clientreqs">
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                                <li>Tristique sed quisque facilisis senectus.</li>
+                                <li>In neque volutpat porta eget amet scelerisque nibh.</li>
+                                <li>Maecenas tempor arcu lacus proin tempor egestas.</li>
+                            </ul>
+                        </div>
+                    </Grid>
+                    <Grid item xs={5}>
+                        <div class="mentorbox">
+                            <header class="mentorheader">Mentored Project</header>
+                            <Grid item xs={11}>
+                                <p className="mentortext">
+                                    <br></br>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat sagittis integer placerat quis metus, nisl, tellus. Sed diam et massa ante id egestas nunc lacus.
+                                    <br></br>
+                                    <br></br>
+                                    Requirements:  
+                                </p>
+                            </Grid>
+
+                            <ul className="mentorreqs">
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                                <li>Tristique sed quisque facilisis senectus.</li>
+                                <li>In neque volutpat porta eget amet scelerisque nibh.</li>
+                                <li>Maecenas tempor arcu lacus proin tempor egestas.</li>
+                            </ul>
+                        </div>
+                    </Grid>
+                </Grid>
+
             
-           
-           
-            <header className="projectteams">Project Teams</header>
-            <div class="clientbox"></div>
-            <header class="clientheader">Client Project</header>
-            <p className="clienttext">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat sagittis integer placerat quis metus, nisl, tellus. Sed diam et massa ante id egestas nunc lacus.
-                <br></br>
-                <br></br>
-                Requirements:  
-            </p>
-            <ul className="clientreqs">
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                <li>Tristique sed quisque facilisis senectus.</li>
-                <li>In neque volutpat porta eget amet scelerisque nibh.</li>
-                <li>Maecenas tempor arcu lacus proin tempor egestas.</li>
-            </ul>
-            <div class="mentorbox"></div>
-            <header class="mentorheader">Mentored Project</header>
-            <p className="mentortext">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat sagittis integer placerat quis metus, nisl, tellus. Sed diam et massa ante id egestas nunc lacus.
-                <br></br>
-                <br></br>
-                Requirements:  
-            </p>
-            <ul className="mentorreqs">
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                <li>Tristique sed quisque facilisis senectus.</li>
-                <li>In neque volutpat porta eget amet scelerisque nibh.</li>
-                <li>Maecenas tempor arcu lacus proin tempor egestas.</li>
-            </ul>
+                <Grid item container>
+                    <Grid item xs={1} />
+                    <Grid item xs={5}>
+                        <header class="quarterheader">What does a typical quarter look like?</header>
+                        <p class="quartertext">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque non, aliquet sed sodales nunc ultricies morbi purus neque. Quam sed porttitor suspendisse nulla non semper sed ipsum. Quam egestas hendrerit nec imperdiet. Vestibulum tortor leo purus suspendisse tortor, mattis ornare nec massa. Cras rutrum augue purus quis arcu vel non. Adipiscing libero orci, ut lorem bibendum.
+                            <br></br>
+                            <br></br>
+                            Feugiat nec turpis sem sagittis nulla adipiscing bibendum sed. Sed eget sed non sollicitudin. Ut sit nulla urna facilisis amet, euismod ut neque placerat. Senectus nunc orci metus montes, enim, lectus quam mi. Varius velit, commodo nisi, ipsum, sed diam. Sed at accumsan scelerisque facilisi nullam in. Commodo nec congue est diam. Sit elit orci, arcu arcu nulla sagittis mauris diam hendrerit.
+                        </p>
+                    </Grid>
+                    <Grid item xs={1} />
+                    <Grid item xs={4}>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <div className="qtrimagespace"></div>
+                    </Grid>
+                </Grid>
+               
+            
+                
 
 
-
-            <header class="quarterheader">What does a typical quarter look like?</header>
-            <p class="quartertext">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque non, aliquet sed sodales nunc ultricies morbi purus neque. Quam sed porttitor suspendisse nulla non semper sed ipsum. Quam egestas hendrerit nec imperdiet. Vestibulum tortor leo purus suspendisse tortor, mattis ornare nec massa. Cras rutrum augue purus quis arcu vel non. Adipiscing libero orci, ut lorem bibendum.
-                <br></br>
-                <br></br>
-                Feugiat nec turpis sem sagittis nulla adipiscing bibendum sed. Sed eget sed non sollicitudin. Ut sit nulla urna facilisis amet, euismod ut neque placerat. Senectus nunc orci metus montes, enim, lectus quam mi. Varius velit, commodo nisi, ipsum, sed diam. Sed at accumsan scelerisque facilisi nullam in. Commodo nec congue est diam. Sit elit orci, arcu arcu nulla sagittis mauris diam hendrerit.
-            </p>
-            <div className="qtrimagespace"></div>
-
-
-        </div>
+            </div>
+        </Grid>
     )
 }
 
