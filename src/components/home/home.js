81fc5627ecc703fcbd2263b822c824b3c2f91928
DanielKim12 <kimgukil2@example.com>
2020-11-24 00:46:10 -0800

    //

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,55 +7,51 @@ import PastWorkCard from "../projects/pastWorkCard.js";
 
 function home(props) { 
     return (
-        <div>
-        <br></br>
-        <br></br>
         <Grid container> 
-            <Grid container item xs={0} md={2}/>
-            <Grid container item xs={12} md={3}>
-                <img class="imagestyle" src={img}/>
-                <label class="toptext">
-                <br></br>
-                <h>
-                    <p className="txtf">
-                        We’re a team of driven, exploring developers at UC Davis, building projects for clients.
-                    </p>
-                </h>
-                </label>
-            </Grid>
-            <Grid container item xs={12} md={3} />
-                <Grid container item xs={3}>
-                    <Grid item>
-                        <label>
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFtndUuH8KTDrEYKGCnyn1-ZOGgZtJ-treQQ&usqp=CAU"/>
-                        </label>
-                    </Grid>
-                </Grid>
-            <Grid container item xs={1} />
-            </Grid>
-            <br></br>
             <Grid container>
-            <Grid container item xs={3} />
-                <div>
+            <Grid container item xs={2} md={2}/>
+            <Grid container item xs={10} md={3}>
+            <div className="space" />
+                <img class="imagestyle"src={img}/>
+            </Grid>
+            </Grid>
+
+            <Grid container>
+                <Grid container item xs ={1} md={2} />
+                <Grid container item xs={11} md={4}>
+                <p class="txtf">
+                    We’re a team of driven, exploring developers at UC Davis, building projects for clients.
+                </p>
+                </Grid>
+                <Grid container item xs={1} md={2}/>
+                <Grid container item xs={11} md={3}>
+                    <div className="circle" />
+                </Grid>
+                <Grid container item xs={0} md={1} />
+            </Grid>
+            
+            <Grid container>
+            <Grid container item xs={2} md={3} />
+                <Grid container item xs={9} md={6}>
                     <p className="toptext"> 
-                    <br></br>
                     We're not just an orgranization, but a team
-                    <br></br>                      
                     and a community, always learning from each other
-                    <br></br>
                     and growing our skills togeter.
                     </p>
-                </div>
+                    </Grid>
+            <Grid container item xs={1} md={3}/>        
             </Grid>
-            <br></br>
-            <br></br>
-            <Grid container item>
-                <Grid container item xs={12} md={6}>
+
+            <Grid container>
+            <Grid container item xs={1} md={1}/>        
+            <Grid container item xs={11} md={5}>
+                <br></br>
                     <div className=  "longbox"/>
                     <br></br>
                     <div className=  "longbox"/>
                 </Grid>
                 <Grid contanier item xs={12} md={6}>
+                <br></br>
                     <div className= "vtbox"/>
                 </Grid>
             </Grid>
@@ -64,50 +60,56 @@ function home(props) {
             <div>
                 <p className="tagtext"> <b>Our Projects</b></p>
 
-            <Grid item container xs={12}>
+            <Grid item container xs={12} md={12}>
                 <p className="subtext">  Here are some of the projects we have worked on in the past. </p>
             </Grid>
             </div>
+            </Grid>
 
             <Grid item>
                 <div>
                     <br></br>
                     <Grid container>
-                    <Grid container iem xs={4}>
+                    <Grid container iem xs={12} md={4}>
                     <PastWorkCard 
                         logo = "logo"
                         companyName= "Company Name" 
                         projectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat sagittis integer placerat quis metus, nisl, tellus. Sed diam et massa ante id egestas nunc lacus."
                     />    
                     </Grid>
-                    <Grid container item xs={3}>
+                    <Grid container item xs={12} md={3}>
                         <div className = "homebox"/>
                     </Grid>
-                    <Grid container item xs={1}/>
-                        <Grid container item xs={3}>
+                    <Grid container item xs={0} md={1}/>
+                        <Grid container item xs={12} md ={3}>
                             <div className = "homebox"/>
                         </Grid>
+                        <Grid container item xs={0} md={1}/>
                     </Grid>
                 </div>
+            </Grid>
 
                 <Grid item>
                     <div>
                         <p className="tagtext"> <b>Join Us! </b></p>
                         <br></br>
-                        <Grid item container xs={12}>
+                        <Grid item container xs={12} md={12}>
                             <p className="subtext">  We recruit twice a year, once in fall quarter and once in spring quarter. </p>
                         </Grid>
                         <br></br>
+                        <Grid contaner>
+                        <Grid container item xs={1} md={1}/>
                         <div class="applybutton">
+                            <Grid container item xs={11} md={11}>
                             <Link class="text" href="/contactus">
-                                <body1 class="buttonui">Apply Here</body1>
+                                <body1 class="applybuttontext">Apply Here</body1>
                             </Link>
+                            </Grid>
                         </div>
+                        </Grid>
                     </div>
                 </Grid>
             </Grid>
-        </Grid>
-    </div>
     )
 }
 
