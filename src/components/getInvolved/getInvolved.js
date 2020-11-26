2969a4dfe40b942f5a8298e7786c05843f1d3080
Tom Abraham <tomabraham6279@gmail.com>
2020-11-25 17:20:16 -0800

    Implemented some REM conversions and mostly done with mobile optimization, still some changes left

--- src/components/getInvolved/getInvolved.js
+++ src/components/getInvolved/getInvolved.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid, Link } from "@material-ui/core";
+import GetInvolvedGraphic1 from "./GetInvolvedGraphic1.png"
 
 function getInvolved(props) {
     return (
@@ -16,60 +17,58 @@ function getInvolved(props) {
                 <Grid item container>
                     <Grid item xs={1} />
                     <Grid item xs={6}>
-                        <p className="getinvolvedsub">There are many different ways to get involved and we would love for you to join us.</p>
+                        <p className="getinvolvedsub">There are a couple ways to get involved and we would love for you to join us.</p>
                     </Grid>
                 </Grid>
 
                 <Grid item container>
-                    <Grid item xs={1} />
+                    <Grid item md={1} />
                     <Grid item xs={6}>
                         <header className="projectteams">Project Teams</header>
                     </Grid>
                 </Grid>
 
                 <Grid item container>
-                    <Grid item xs={1} />
-                    <Grid item xs={5}>
+                    <Grid item md={1} />
+                    <Grid item xs={11} md={5}>
                         <div class="clientbox">
                             <header class="clientheader">Client Project</header>
                             <Grid item xs={11}>
                                 <p className="clienttext">
                                     <br></br>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat sagittis integer placerat quis metus, nisl, tellus. Sed diam et massa ante id egestas nunc lacus.
+                                    Client project developers work on projects with companies and nonprofits. Client project developers should have more extensive programming experience. 
                                     <br></br>
                                     <br></br>
                                      Requirements:  
                                 </p>
                             </Grid>
-                            
-                             <ul className="clientreqs">
-                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                                <li>Tristique sed quisque facilisis senectus.</li>
-                                <li>In neque volutpat porta eget amet scelerisque nibh.</li>
-                                <li>Maecenas tempor arcu lacus proin tempor egestas.</li>
-                            </ul>
+                            <Grid item xs={11}>
+                                <ul className="clientreqs">
+                                    <li>Taken or currently enrolled in ECS 36A, or equivalent programming experience.</li>
+                                    <li>Completed a larger scale coding project (side project, industry experience, CodeLab project).</li>
+                                </ul>
+                            </Grid>  
                         </div>
                     </Grid>
                     <Grid item xs={1} />
-                    <Grid item xs={5}>
+                    <Grid item xs={11} md={5}>
                         <div class="mentorbox">
                             <header class="mentorheader">Mentored Project</header>
                             <Grid item xs={11}>
                                 <p className="mentortext">
                                     <br></br>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat sagittis integer placerat quis metus, nisl, tellus. Sed diam et massa ante id egestas nunc lacus.
+                                    For students with less programming experience, mentored project developers work on smaller projects under the guidance of a Project Mentor.
                                     <br></br>
                                     <br></br>
                                     Requirements:  
                                 </p>
                             </Grid>
-
-                            <ul className="mentorreqs">
-                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                                <li>Tristique sed quisque facilisis senectus.</li>
-                                <li>In neque volutpat porta eget amet scelerisque nibh.</li>
-                                <li>Maecenas tempor arcu lacus proin tempor egestas.</li>
-                            </ul>
+                            <Grid item xs={11}>
+                                <ul className="mentorreqs">
+                                    <li>Taken or currently enrolled in ECS 32A, or equivalent programming experience.</li>
+                                    <li>Programming experience beyond coursework is great, but not a requirement.</li>
+                                </ul>
+                            </Grid>  
                         </div>
                     </Grid>
                 </Grid>
@@ -77,23 +76,34 @@ function getInvolved(props) {
             
                 <Grid item container>
                     <Grid item xs={1} />
-                    <Grid item xs={5}>
-                        <header class="quarterheader">What does a typical quarter look like?</header>
+                    <Grid item xs={7} md={5}>
+                        <header class="quarterheader">What does a typical term look like?</header>
                         <p class="quartertext">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque non, aliquet sed sodales nunc ultricies morbi purus neque. Quam sed porttitor suspendisse nulla non semper sed ipsum. Quam egestas hendrerit nec imperdiet. Vestibulum tortor leo purus suspendisse tortor, mattis ornare nec massa. Cras rutrum augue purus quis arcu vel non. Adipiscing libero orci, ut lorem bibendum.
+                            Each year in CodeLab has two terms, a Fall term and a Winter/Spring term. The Fall term runs approximately 7 weeks in fall quarter, and the Winter/Spring term runs approximately 16 weeks across winter and spring quarter. This means the scale of projects will vary between terms. Recruitment occurs at the beginning of each term.
                             <br></br>
                             <br></br>
-                            Feugiat nec turpis sem sagittis nulla adipiscing bibendum sed. Sed eget sed non sollicitudin. Ut sit nulla urna facilisis amet, euismod ut neque placerat. Senectus nunc orci metus montes, enim, lectus quam mi. Varius velit, commodo nisi, ipsum, sed diam. Sed at accumsan scelerisque facilisi nullam in. Commodo nec congue est diam. Sit elit orci, arcu arcu nulla sagittis mauris diam hendrerit.
+                            Over the course of the term, developers will have a series of meetings and presentations while working on their projects. Below is a general outline of a CodeLab term.
                         </p>
+                            <ol>
+                                <li>First General Meeting</li>
+                                <li>Project Overview Meeting w/ Client (Client Teams)</li>
+                                <li>Education Week</li>
+                                <li>Midterm Presentations</li>
+                                <li>Final Presentations</li>
+                                <li>Handoff Meeting w/ Client (Client Teams)</li>
+                            </ol>
+                        
                     </Grid>
-                    <Grid item xs={1} />
-                    <Grid item xs={4}>
-                        <div className="qtrimagespace"></div>
+                    <Grid item md={1} />
+                    <Grid item xs={8} md={4}>
+                        <div class="qtrimagespace">
+                            <img src={GetInvolvedGraphic1} alt="Get Involved!"></img>
+                        </div>
                     </Grid>
                 </Grid>
 
                 <Grid item container>
-                    <Grid item xs={1} />
+                    <Grid item md={1} />
                     <Grid item xs={11}>
                         <header class="faqheader">Frequently Asked Questions</header>
                     </Grid>
@@ -101,68 +111,52 @@ function getInvolved(props) {
 
                 <Grid item container>
                     <Grid item xs={1} />
-                    <Grid item xs={5} direction="column">
-                        <header class="faqsubheader">Feugiat nec turpis sem sagittis nulla adipiscing bibendum sed?</header>
-                        <p class="faqtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque non, aliquet sed sodales nunc ultricies morbi purus neque. Quam sed porttitor suspendisse nulla non semper sed ipsum. Quam egestas hendrerit nec imperdiet. Vestibulum tortor leo purus suspendisse tortor, mattis ornare nec massa. Cras rutrum augue purus quis arcu vel non. Adipiscing libero orci, ut lorem bibendum.</p>
+                    <Grid item xs={7} md={5} direction="column">
+                        <header class="faqsubheader">Do I need to be a computer science major to apply to CodeLab?</header>
+                        <p class="faqtext">Nope! We are open to undergraduate students at UC Davis of any major. You do need to have some programming experience to join. Detailed requirements are above.</p>
                     </Grid>
 
                     <Grid item xs={1} />
-
-                    <Grid item xs={5} direction="column">
-                        <header class="faqsubheader">Feugiat nec turpis sem sagittis nulla adipiscing bibendum sed?</header>
-                        <p class="faqtext"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque non, aliquet sed sodales nunc ultricies morbi purus neque. Quam sed porttitor suspendisse nulla non semper sed ipsum. Quam egestas hendrerit nec imperdiet. Vestibulum tortor leo purus suspendisse tortor, mattis ornare nec massa. Cras rutrum augue purus quis arcu vel non. Adipiscing libero orci, ut lorem bibendum. Quam egestas hendrerit nec imperdiet. Vestibulum tortor leo purus suspendisse tortor, mattis ornare nec massa. Cras rutrum augue purus quis arcu vel non. Adipiscing libero orci, ut lorem bibendum.</p>
-                    </Grid>
-                </Grid>
-
-                <Grid item container>
-                    <Grid item xs={1} />
-                    <Grid item xs={5} direction="column">
-                        <header class="faqsubheader">Feugiat nec turpis sem sagittis nulla adipiscing bibendum sed?</header>
-                        <p class="faqtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque non, aliquet sed sodales nunc ultricies morbi purus neque. Quam sed porttitor suspendisse nulla non semper sed ipsum.</p>
-                    </Grid>
-
-                    <Grid item xs={1} />
-
-                    <Grid item xs={5} direction="column">
-                        <header class="faqsubheader">Feugiat nec turpis sem sagittis nulla adipiscing bibendum sed?</header>
-                        <p class="faqtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque non, aliquet sed sodales nunc ultricies morbi purus neque. Quam sed porttitor suspendisse nulla non semper sed ipsum. Quam egestas hendrerit nec imperdiet. Vestibulum tortor leo purus suspendisse tortor, mattis ornare nec massa. Cras rutrum augue purus quis arcu vel non. Adipiscing libero orci, ut lorem bibendum.</p>
+                    <Grid item xs={7} md={5} direction="column">
+                        <header class="faqsubheader">What types of projects do developers work on?</header>
+                        <p class="faqtext">Anything computer science, whether it be web development or data science. Projects are largely reliant on the needs of our clients from term to term. Mentored projects can often be more flexible in the computer science fields they involve.</p>
                     </Grid>
                 </Grid>
 
                 <Grid item container>
                     <Grid item xs={1} />
-                    <Grid item xs={5} direction="column">
-                        <header class="faqsubheader">Feugiat nec turpis sem sagittis nulla adipiscing bibendum sed?</header>
-                        <p class="faqtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque non, aliquet sed sodales nunc ultricies morbi purus neque. Quam sed porttitor suspendisse nulla non semper sed ipsum. Quam egestas hendrerit nec imperdiet. Vestibulum tortor leo purus suspendisse tortor, mattis ornare nec massa. Cras rutrum augue purus quis arcu vel non. Adipiscing libero orci, ut lorem bibendum. Quam egestas hendrerit nec imperdiet. Vestibulum tortor leo purus suspendisse tortor, mattis ornare nec massa. Cras rutrum augue purus quis arcu vel non. Adipiscing libero orci, ut lorem bibendum.</p>
+                    <Grid item xs={7} md={5} direction="column">
+                        <header class="faqsubheader">What is the time commitment like?</header>
+                        <p class="faqtext">Developers are expected to put in 10+ hours a week. Depending on the part of the project you are working, this may fluctuate throughout the term. Being a developer can be time consuming. Before applying, please consider your other commitments to make sure you will be able to put in the time for CodeLab.</p>
                     </Grid>
 
+                    <Grid item xs={1} />
+
+                    <Grid item xs={7} md={5} direction="column">
+                        <header class="faqsubheader">What does the recruitment process look like?</header>
+                        <p class="faqtext">The process currently involves a written application and a single round of interviews. Don’t be nervous about interviews. We just want to get to know you and learn about your past experiences. It will not be technical.</p>
+                    </Grid>
                 </Grid>
 
                 <Grid item container>
-                    <Grid item xs={1} />
+                    <Grid item xs={2} md={1} />
                     <Grid item xs={11}>
                         <header class="readytoapply">Ready to Apply?</header>
                     </Grid>
-                    <Grid item xs={1} />
-                    <Grid item xs={8}>
-                        <p class="applytext">We’re currently taking applications for the Winter 2021 term. Applications are due Wednesday, January 13th by 11:59PM.</p>
+                    <Grid item xs={2} md={1} />
+                    <Grid item xs={5} md={8}>
+                        <p class="applytext">We’re currently taking applications for the Winter/Spring 2021 term. Applications are due Thursday, January 14th by 11:59PM.</p>
                     </Grid>
                 </Grid>
 
                 <Grid item container>
-                    <Grid item xs={1} />
-                    <div class="applybutton">
-                        <Grid item xs={11}>
+                    <Grid item xs={2} md={1} />
+                    <Grid item xs={4}md={3}>
+                        <div class="applybutton">
                             <p class="applybuttontext">Apply Here</p>
-                        </Grid>
-                    </div>
+                        </div>
+                    </Grid>
                 </Grid>
-                
-               
-            
-                
-
-
             </div>
         </Grid>
     )
