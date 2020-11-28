7e7bdd462b5c10009a0e7089e88b7db24a55cd87
edoC16 <43505068+edoC-16@users.noreply.github.com>
2020-11-27 18:23:16 -0800

    Week 8 changes

--- src/components/contactUs/contactUs.js
+++ src/components/contactUs/contactUs.js
@@ -1,50 +1,51 @@
 import React from "react";
-import "./contactUs.css"
-import InputBox from "./inputBox"
+import "./contactUs.css";
+import InputBox from "./inputBox";
 import { Grid } from "@material-ui/core";
-{/*import { Input } from 'semantic-ui-react';*/}
+{
+  /*import { Input } from 'semantic-ui-react';*/
+}
 
 function contactUs(props) {
   return (
     <Grid container>
-      <Grid container item xs={12}>
+      <Grid container item xs={12} md={12}>
         <div className="contactTitle"> contact us. </div>
-        <Grid container item xs={12}>
-        <h className="contactq"> You've got questions, we've got answers. </h>
+        <Grid container item xs={12} md={12}>
+          <h className="contactq"> You've got questions, we've got answers. </h>
+        </Grid>
       </Grid>
-      </Grid>
-      
-      <Grid container item xs={12}>
+
+      <Grid container item xs={12} md={12}>
         <div>
           <p className="chatTitle"> Let's chat!</p>
         </div>
       </Grid>
-      <Grid container item xs={3}>
-        <p className="chatstyle">
-          Whether you’re a potential client 
-          or a student with questions, shoot us 
-          a message through this form!
+      <Grid container item xs={12} md={3}>
+        <p className="chatTxt">
+          Whether you’re a potential client or a student with questions, shoot
+          us a message through this form!
         </p>
       </Grid>
-      <Grid container item xs={1}></Grid>
-      <Grid container item xs={4}> 
-        <InputBox width="333px" height ="53px" prompt="Name"/>
-      </Grid> 
-      <Grid container item xs={4}> 
-        <InputBox width="333px" height ="53px" prompt="Email Address"/>
+      <Grid container item xs={0} md={1}></Grid>
+
+      <div className="space" />
+
+      <Grid container item xs={12} md={4}>
+        <InputBox width="21rem" height="3rem" prompt="Name" />
       </Grid>
-      <Grid container item xs ={3}>
-          <p className="chatstyle">
-            You can also reach us at
-            daviscodelab@gmail.com.
-          </p>
-    </Grid>
-    <Grid container item xs={1}></Grid>
-    <Grid container item xs={8}>
-      <div>
-        <InputBox width= "753px" height = "232px" prompt="Message"/>
-      </div>
-    </Grid>
+      <Grid container item xs={4} md={4}>
+        <InputBox width="21rem" height="3rem" prompt="Email Address" />
+      </Grid>
+      <Grid container item xs={12} md={3}>
+        <p className="chatTxt">
+          You can also reach us at daviscodelab@gmail.com.
+        </p>
+      </Grid>
+      <Grid container item xs={0} md={1}></Grid>
+      <Grid container item xs={12} md={8}>
+          <InputBox width="100%" height="15rem" prompt="Message" />
+      </Grid>
     </Grid>
   );
 }
