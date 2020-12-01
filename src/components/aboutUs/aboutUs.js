9cfadcddb7e8a3e8e9a56d8f4d6b3166ea8b6a82
Omid <omidmogasemi@gmail.com>
2020-11-30 17:01:51 -0800

    About Us Page Cleanup

--- src/components/aboutUs/aboutUs.js
+++ src/components/aboutUs/aboutUs.js
@@ -1,72 +1,132 @@
 import React from "react";
 import "../../css/aboutUs.css"
 import { Grid } from "@material-ui/core";
-
-
-
-
+import groupphoto from "./images/GroupPhoto1.png"
+import essam from "./images/essam.png"
+import omid from "./images/omid.png"
+import james from "./images/james.png"
+import edward from "./images/edward.png"
+import tom from "./images/tom.png"
+import chris from "./images/chris.png"
+import guk from "./images/guk.png"
+import andy from "./images/andy.png"
+import vishnu from "./images/vishnu.png"
+import jeffrey from "./images/jeffrey.png"
+import henry from "./images/henry.png"
 
 const about =()=> {
     return (
         <Grid container>
-            <Grid container item xs={6}>
-                <b>
-                    <h1 className = "aboutus">
+            <Grid container>
+                <Grid container item xs={1} />
+                <Grid container item xs={10} md={4}>
+                    <h1 className="aboutus">
                         about us.
                     </h1>
-
-                </b>
+                </Grid>
+                <Grid container item xs={1}/>
+                <Grid container item xs={1}/>
+                <Grid container item xs={10} md={5}>
+                    <img src={groupphoto} className="topimage" alt='' />
+                </Grid>
             </Grid>
-            <div className = "groupPhoto" >
-                <img src = "/aboutImages/GroupPhoto 1.png" alt='' />
-            </div> 
-            <Grid container item xs={12} justify="center">
+            <Grid container item xs={1} md={2} />
+            <Grid container item xs={10} md={8} justify="center">
                 <h2 className = "aboutustxt" >
-                Hey, we’re Codelab. The four of us—Essam, Omid, James, and Edward—went through our first year at UC Davis feeling like there wasn’t enough of a computer science community. With heavily theoretical coursework, it could be difficult to develop the skills associated with real world applications of our coding knowledge. {"\n"}
-
-                And so, we built CodeLab, a software development organization at UC Davis. In CodeLab, developers work in small teams on real-world projects, from web development to data science. Client Projects provide devlopers with the opportunity to contribute to companies, and Mentored Projects allow developers to learn new skills under the guidance of an experienced student. {"\n"}
-
+                Hey, we’re Codelab. The four of us—Essam, Omid, James, and Edward—went through our first year at UC Davis feeling like there wasn’t enough of a computer science community. With heavily theoretical coursework, it could be difficult to develop the skills associated with real world applications of our coding knowledge. <br /><br />
+                And so, we built CodeLab, a software development organization at UC Davis. In CodeLab, developers work in small teams on real-world projects, from web development to data science. Client Projects provide devlopers with the opportunity to contribute to companies, and Mentored Projects allow developers to learn new skills under the guidance of an experienced student. <br /><br />
                 Today, we are a team of driven computer science students creating pieces of code we are proud of. And through it all, we are always learning and always innovating.
                 </h2>
             </Grid>
+            <Grid container item xs={1} md={2} />
 
-            <h3 className = "team" >
-                Our Team
-            </h3>
-      
-            <div className = "teamPhoto1" >
-            <img src = "/aboutImages/Group 8.png" alt='' />
-            </div>
-            <div className = "teamPhoto2" >
-            <img src = "/aboutImages/Group 9.png" alt='' />
-            </div>
-            <div className = "teamPhoto3" >
-            <img src = "/aboutImages/Group 10.png" alt='' />
-            </div>
-            <div className = "teamPhoto4" >
-            <img src = "/aboutImages/Group 11.png" alt='' />
-            </div>
-            <div className = "teamPhoto5" >
-            <img src = "/aboutImages/Group 12.png" alt='' />
-            </div>
-            <div className = "teamPhoto6" >
-            <img src = "/aboutImages/Group 13.png" alt='' />
-            </div>
-            <div className = "teamPhoto7" >
-            <img src = "/aboutImages/Group 14.png" alt='' />
-            </div>
-            <div className = "teamPhoto8" >
-            <img src = "/aboutImages/Group 15.png" alt='' />
-            </div>
-            <div className = "teamPhoto9" >
-            <img src = "/aboutImages/Group 17.png" alt='' />
-            </div>
-            <div className = "teamPhoto10" >
-            <img src = "/aboutImages/Group 18.png" alt='' />
-            </div>
-            <div className = "teamPhoto11" >
-            <img src = "/aboutImages/Group 19.png" alt='' />
-            </div>
+            <Grid container xs={1} />
+            <Grid container xs={10}>
+                <Grid container item xs={12}>
+                    <h3 className="teamtoptext">
+                        Our Team
+                    </h3>
+                </Grid>
+
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={essam} alt='' />
+                        <body1 className="name">Essam Sleiman <br /></body1> 
+                        <body1 className="title">President</body1>
+                    </div>
+                </Grid>
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={omid} alt='' />
+                        <body1 className="name">Omid Mogasemi<br /></body1> 
+                        <body1 className="title">VP of Projects</body1>       
+                     </div>
+                </Grid>
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={james} alt='' />
+                        <body1 className="name">James Junaidi <br /></body1> 
+                        <body1 className="title">VP of Operations</body1>      
+                    </div>
+                </Grid>
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={edward} alt='' />
+                        <body1 className="name">Edward Chew <br /></body1> 
+                        <body1 className="title">VP of External</body1>               
+                    </div>
+                </Grid>
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={tom} alt='' />
+                        <body1 className="name">Tom Abraham <br /></body1> 
+                        <body1 className="title">Developer</body1> 
+                    </div>
+                </Grid>
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={chris} alt='' />
+                        <body1 className="name">Chris Havens <br /></body1> 
+                        <body1 className="title">Developer</body1>      
+                     </div>
+                </Grid>
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={guk} alt='' />
+                        <body1 className="name">Guk Kim <br /></body1> 
+                        <body1 className="title">Developer</body1>              
+                    </div>
+                </Grid>
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={andy} alt='' />
+                        <body1 className="name">Andy Ren <br /></body1> 
+                        <body1 className="title">Developer</body1>              
+                    </div>
+                </Grid>
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={vishnu} alt='' />
+                        <body1 className="name">Vishnu Vennelakanti <br /></body1> 
+                        <body1 className="title">Developer</body1>                   
+                    </div>
+                </Grid>
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={jeffrey} alt='' />
+                        <body1 className="name">Jeffrey Zang <br /></body1> 
+                        <body1 className="title">Developer</body1> 
+                    </div>
+                </Grid>
+                <Grid container item xs={12} md={3} style={{marginBottom: "0.5rem"}}>
+                    <div className="teamPhoto" >
+                        <img src={henry} alt='' />
+                        <body1 className="name">Henry Zhu <br /></body1> 
+                        <body1 className="title">Developer</body1> 
+                    </div>
+                </Grid>
+            </Grid>
+            <Grid container xs={1} />
         </Grid>
     )
 }
