57bb6293f9955b7101ee033065dded4fa16a928e
Tom Abraham <tomabraham6279@gmail.com>
2020-12-03 17:26:57 -0800

    Finished most mobile optimization, still need to fix image overflow

--- src/components/getInvolved/getInvolved.js
+++ src/components/getInvolved/getInvolved.js
@@ -22,8 +22,8 @@ function getInvolved(props) {
                 </Grid>
 
                 <Grid item container>
-                    <Grid item md={1} />
-                    <Grid item xs={6}>
+                    <Grid item xs={2} md={1} />
+                    <Grid item xs={7} md={6} justify="center">
                         <header className="projectteams">Project Teams</header>
                     </Grid>
                 </Grid>
@@ -75,16 +75,16 @@ function getInvolved(props) {
 
             
                 <Grid item container>
-                    <Grid item xs={1} />
-                    <Grid item xs={7} md={5}>
+                    <Grid item md={1} />
+                    <Grid item xs={8} md={5} justify="center">
                         <header class="quarterheader">What does a typical term look like?</header>
-                        <p class="quartertext">
+                        <p class="quartertext" justify="center">
                             Each year in CodeLab has two terms, a Fall term and a Winter/Spring term. The Fall term runs approximately 7 weeks in fall quarter, and the Winter/Spring term runs approximately 16 weeks across winter and spring quarter. This means the scale of projects will vary between terms. Recruitment occurs at the beginning of each term.
                             <br></br>
                             <br></br>
                             Over the course of the term, developers will have a series of meetings and presentations while working on their projects. Below is a general outline of a CodeLab term.
                         </p>
-                            <ol>
+                            <ol class="meetings">
                                 <li>First General Meeting</li>
                                 <li>Project Overview Meeting w/ Client (Client Teams)</li>
                                 <li>Education Week</li>
@@ -94,8 +94,8 @@ function getInvolved(props) {
                             </ol>
                         
                     </Grid>
-                    <Grid item md={1} />
-                    <Grid item xs={8} md={4}>
+                    <Grid item xs={4} md={1} />
+                    <Grid item xs={3} md={4}>
                         <div class="qtrimagespace">
                             <img src={GetInvolvedGraphic1} alt="Get Involved!"></img>
                         </div>
@@ -110,30 +110,30 @@ function getInvolved(props) {
                 </Grid>
 
                 <Grid item container>
-                    <Grid item xs={1} />
-                    <Grid item xs={7} md={4} direction="column">
+                    <Grid item md={1} />
+                    <Grid item xs={8} md={4} direction="column" justify="center">
                         <header class="faqsubheader">Do I need to be a computer science major to apply to CodeLab?</header>
                         <p class="faqtext">Nope! We are open to undergraduate students at UC Davis of any major. You do need to have some programming experience to join. Detailed requirements are above.</p>
                     </Grid>
 
-                    <Grid item xs={2} />
+                    <Grid item md={2} />
                     
-                    <Grid item xs={7} md={4} direction="column">
+                    <Grid item xs={8} md={4} direction="column" justify="center">
                         <header class="faqsubheader">What types of projects do developers work on?</header>
                         <p class="faqtext">Anything computer science, whether it be web development or data science. Projects are largely reliant on the needs of our clients from term to term. Mentored projects can often be more flexible in the computer science fields they involve.</p>
                     </Grid>
                 </Grid>
 
                 <Grid item container>
-                    <Grid item xs={1} />
-                    <Grid item xs={7} md={4} direction="column">
+                    <Grid item md={1} />
+                    <Grid item xs={8} md={4} direction="column" justify="center">
                         <header class="faqsubheader">What is the time commitment like?</header>
                         <p class="faqtext">Developers are expected to put in 10+ hours a week. Depending on the part of the project you are working, this may fluctuate throughout the term. Being a developer can be time consuming. Before applying, please consider your other commitments to make sure you will be able to put in the time for CodeLab.</p>
                     </Grid>
 
                     <Grid item xs={2} />
 
-                    <Grid item xs={7} md={4} direction="column">
+                    <Grid item xs={8} md={4} direction="column" justify="center">
                         <header class="faqsubheader">What does the recruitment process look like?</header>
                         <p class="faqtext">The process currently involves a written application and a single round of interviews. Don’t be nervous about interviews. We just want to get to know you and learn about your past experiences. It will not be technical.</p>
                     </Grid>
@@ -141,18 +141,18 @@ function getInvolved(props) {
 
                 <Grid item container>
                     <Grid item xs={2} md={1} />
-                    <Grid item xs={11}>
+                    <Grid item xs={5} md={11}>
                         <header class="readytoapply">Ready to Apply?</header>
                     </Grid>
-                    <Grid item xs={2} md={1} />
-                    <Grid item xs={5} md={8}>
+                    <Grid item xs={0} md={1} />
+                    <Grid item xs={10} md={8} justify="center">
                         <p class="applytext">We’re currently taking applications for the Winter/Spring 2021 term. Applications are due Thursday, January 14th by 11:59PM.</p>
                     </Grid>
                 </Grid>
 
                 <Grid item container>
                     <Grid item xs={2} md={1} />
-                    <Grid item xs={4}md={3}>
+                    <Grid item xs={4} md={3}>
                         <Link href="/contactus">
                             <button className="applyButton">
                                 <body1>Apply Here</body1>
